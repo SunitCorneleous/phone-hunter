06163b7df9fdcd118ed29d5bf3875f4221c9a086
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-08-30 13:38:13 +0600

    files added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const loadPhonesData = async () => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+};
+
+loadPhonesData();
